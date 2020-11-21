0c76c0f6eab037875eba4649bd7457f6f7730df3
Bhagi <puppyreddy1997@gmail.com>
2020-11-21 20:26:53 +0530

    [Bhagyalaxmi]ADDED:Added code to Calculate Empwage for employee

--- EmpWage.cs
+++ EmpWage.cs
@@ -7,17 +7,25 @@ namespace EmpWageProgram
         static void Main(string[] args)
         {
             int FULL_TIME = 1;
-            Console.WriteLine("WelCome to Empolyee Wage Computation Program");
+            int EMP_WAGE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-            int empcheck = random.Next(0,2);
+            int empcheck = random.Next(0, 2);
+            Console.WriteLine("WelCome to Empolyee Wage Computation Program");
+            
             if(empcheck == FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_WAGE_PER_HR;
+            Console.WriteLine("Employee wage is :" + empWage);
         }
     }
 }
