ac60ca72776a9aba596d5b55d50e2817ac101fa7
Bhagi <puppyreddy1997@gmail.com>
2020-11-21 20:13:56 +0530

    [Bhagyalaxmi]ADDED:Added code to Check emp is present or absent using Random

--- EmpWage.cs
+++ EmpWage.cs
@@ -6,7 +6,18 @@ namespace EmpWageProgram
     {
         static void Main(string[] args)
         {
+            int FULL_TIME = 1;
             Console.WriteLine("WelCome to Empolyee Wage Computation Program");
+            Random random = new Random();
+            int empcheck = random.Next(0,2);
+            if(empcheck == FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
