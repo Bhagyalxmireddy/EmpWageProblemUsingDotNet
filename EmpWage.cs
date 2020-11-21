b8f590d1ef32d9a61a3e39bd1b1832957885091b
Bhagi <puppyreddy1997@gmail.com>
2020-11-21 20:42:13 +0530

    [Bhagyalaxmi]ADDED:Added code to Add a PartTime employee and cal empwage

--- EmpWage.cs
+++ EmpWage.cs
@@ -7,17 +7,23 @@ namespace EmpWageProgram
         static void Main(string[] args)
         {
             int FULL_TIME = 1;
+            int PART_TIME = 2;
             int EMP_WAGE_PER_HR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
             Console.WriteLine("WelCome to Empolyee Wage Computation Program");
             
             if(empcheck == FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Present and Employee is Fulltime");
                 empHrs = 8;
+            }
+             else if(empcheck == 2)
+            {
+                Console.WriteLine("Employee is Present and Employee is Parttime");
+                empHrs = 4;
             }
             else
             {
