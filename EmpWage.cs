74bfb7bae3488ae29729d13822e4bf7b4dd2f06a
Bhagi <puppyreddy1997@gmail.com>
2020-11-21 20:05:33 +0530

    [Bhagyalaxmi]ADDED:Added Console class to print Welcome msg

---
+++ EmpWage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageProgram
+{
+    class EmpWage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to Empolyee Wage Computation Program");
+        }
+    }
+}
