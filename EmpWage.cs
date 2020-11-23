feff67b4ce749eee8fbdb249e96f6f39bcaf13f9
Bhagi <puppyreddy1997@gmail.com>
2020-11-23 23:12:23 +0530

    [Bhagyalaxmi]ADDED:Added code Saving the totalWage for eachCompany

--- EmpWage.cs
+++ EmpWage.cs
@@ -4,47 +4,12 @@ namespace EmpWageProgram
 {
     class EmpWage
     {
-        public const int FULL_TIME = 1;
-        public const int PART_TIME = 2;
-       // public const int NUM_OF_WORKING_DAYS = 20;
-       // public const int EMP_RATE_PER_HR = 20;
-       // public const int MAX_HRS_IN_MONTH = 100;
-        /// <summary>
-        /// Computes the emp wage.
-        /// </summary>
-        /// <returns></returns>
-        public static int computeEmpWage(String companyName, int empRatePerHr,int numOfWorkingDays,int maxHrsPerMonth)
-        {
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            Console.WriteLine("WelCome to Empolyee Wage Computation Program");
-            while (totalEmpHrs <= maxHrsPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empcheck = random.Next(0, 3);
-                switch (empcheck)
-                {
-                    case 1:
-                        empHrs = 8;
-                        break;
-                    case 2:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day : " + totalWorkingDays + " Emp Hrs: " + empHrs);
-            }
-            int totalEmpWage = totalEmpHrs * empRatePerHr;
-            Console.WriteLine("Total Employee wage is :" + totalEmpWage);
-            return totalEmpWage;
-        }
         static void Main(string[] args)
         {
-            computeEmpWage("ABCD",20,5,20);
+            Console.WriteLine("WelCome to Empolyee Wage Computation Program");
+            EmpWageObject bridgeLabz = new EmpWageObject("BridgeLabz", 20, 10, 40);
+            bridgeLabz.computeEmpWage();
+            Console.WriteLine(bridgeLabz.toString());
         }
     }
 }
---
+++ EmpWageObject.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageProgram
+{
+    class EmpWageObject
+    {
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+        // public const int NUM_OF_WORKING_DAYS = 20;
+        // public const int EMP_RATE_PER_HR = 20;
+        // public const int MAX_HRS_IN_MONTH = 100;
+        public int totalEmpWage;
+        private string companyName;
+        private int empRatePerHr;
+        private int numOfWorkingDays;
+        private int maxHrsPerMonth;
+        public EmpWageObject(String companyName, int empRatePerHr, int numOfWorkingDays, int maxHrsPerMonth)
+        {
+            this.companyName = companyName;
+            this.empRatePerHr = empRatePerHr;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHrsPerMonth = maxHrsPerMonth;
+        }
+        /// <summary>
+        /// Computes the emp wage.
+        /// </summary>
+        /// <returns></returns>
+        public void computeEmpWage()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= this.maxHrsPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case 1:
+                        empHrs = 8;
+                        break;
+                    case 2:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day : " + totalWorkingDays + " Emp Hrs: " + empHrs);
+            }
+            totalEmpWage = totalEmpHrs * this.empRatePerHr;
+            Console.WriteLine("Total Employee wage is :" + totalEmpWage);
+        }
+        public string toString()
+        {
+            return "Total EmpWage for Company: " + this.companyName + " is: " + totalEmpWage;
+        }
+        
+    }
+}
