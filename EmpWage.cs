2e566ecc3d19f194a0b9b4aeaef6789e7a74cd69
Bhagi <puppyreddy1997@gmail.com>
2020-11-24 09:19:48 +0530

    [Bhagyalaxmi]ADDED:Added code Ability to manage EmpWage of Multiple Companies

--- EmpWage.cs
+++ EmpWage.cs
@@ -7,9 +7,10 @@ namespace EmpWageProgram
         static void Main(string[] args)
         {
             Console.WriteLine("WelCome to Empolyee Wage Computation Program");
-            EmpWageObject bridgeLabz = new EmpWageObject("BridgeLabz", 20, 10, 40);
-            bridgeLabz.computeEmpWage();
-            Console.WriteLine(bridgeLabz.toString());
+            EmpWageBuliderArray empWageBulider = new EmpWageBuliderArray();
+            empWageBulider.addCompanyEmpWage("BridgeLabz", 20, 5, 40);
+            empWageBulider.computeEmpWage();
+           
         }
     }
 }
